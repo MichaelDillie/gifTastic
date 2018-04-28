842f932ba45fa12032c73eb29a02e0cef8026b1a
Michael Dillie <michaeldillie@gmail.com>
2018-04-28 18:03:31 -0500

    Buttons are now being displayed onto the DOM

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-    
+
+    var gifArray = ["cat", "dog", "hamster", "duck", "mouse", "pig", "rabbit", "frog", "fox", "tiger", "bear", "lizard"];
+
+    var gifBtnRow = $(".gif-btn-row");
+
+
+    function generateBtns() {
+        for(var i = 0; i <gifArray.length; i++) {
+            console.log(gifArray[i]);
+            var gifBtn = $("<a>");
+            gifBtn.addClass("waves-effect waves-light btn gif-btn");
+            gifBtn.text(gifArray[i]);
+            gifBtn.attr("data-gifvalue", gifArray[i]);
+            gifBtnRow.append(gifBtn);
+        }
+    }
+    generateBtns();
+
 });
